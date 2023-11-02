17294d5f5b72bfec2cff2d4832d2aee0889f63c2
Minja Survonen <minja.survonen@tuni.fi>
2023-11-02 19:20:58 +0200

    Showing recommendations for user and adding documentation

--- assignment 1/assignment1.py
+++ assignment 1/assignment1.py
@@ -1,19 +1,16 @@
 import numpy as np
 import pandas as pd
 
+# Reading data from files
 movies = pd.read_csv("ml-latest-small/movies.csv")
 ratings = pd.read_csv("ml-latest-small/ratings.csv",usecols=range(3))
-# all = pd.merge(movies, ratings)
-print(ratings.head(3))
-print(ratings.count())
-
 
 # Data to form where movieIds are columns and each row presents all ratings of one user
 ratings_pivot = ratings.pivot(index='userId', columns='movieId', values='rating')
-ratings_pearson_correlation = ratings_pivot.T.corr('pearson')
+
 
 def search_nearest_neighbors(similarity_data, user_id, amount_of_neighbors):
-    neighbors = similarity_data[user_id].sort_values(ascending=False).iloc[2:amount_of_neighbors+2]
+    neighbors = similarity_data[user_id].sort_values(ascending=False).iloc[1:amount_of_neighbors+1]
     return (neighbors)
 
 def search_ratings_of_neighbors(rating_data,neighbor_data):
@@ -43,12 +40,31 @@ def predict_movie_score(selected_user_ratings, neighbors, ratings_of_neighbors):
 
     return prediction_for_user
 
+
+print(ratings.head(3))
+print(ratings.count())
+
+# Similarity of users with Pearson correlation
+ratings_pearson_correlation = ratings_pivot.T.corr('pearson')
+print(ratings_pearson_correlation)
+
+# Selecting user and searching 10 nearest users
 selected_user = 249
 neighbors = search_nearest_neighbors(ratings_pearson_correlation,selected_user,10)
 ratings_of_neighbors = search_ratings_of_neighbors(ratings_pivot, neighbors)
-#print(ratings_of_neighbors)
+print(ratings_of_neighbors)
 
+# Ratings of selected user
 selected_user_ratings = ratings_pivot.loc[selected_user]
 print(selected_user_ratings)
+
+# Counting predictions to all movies that are rated any of neighbors
 prediction_of_movies = predict_movie_score(selected_user_ratings, neighbors, ratings_of_neighbors)
-print(prediction_of_movies)
+highest_predictions = prediction_of_movies.sort_values(ascending=False).head(10)
+# MovieIds of top 10 predictions 
+highest_predictions_ids = highest_predictions.index
+
+# Printing names of 10 highest predictions for selected user
+for prediction in highest_predictions_ids:
+    movie = movies.query('movieId == @prediction')
+    print(movie['t'])
